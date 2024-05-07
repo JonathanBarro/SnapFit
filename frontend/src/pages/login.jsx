893b97f7ce5fc2fff2031f311af59dd4fa93c541
Jonathan Barroeta Lasa <jbarroeta@opendeusto.es>
2024-05-07 02:52:25 +0200

    Login
    Botón para visualizar la contraseña

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,23 +1,20 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const LogIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false); // Estado para controlar la visibilidad de la contraseña
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleLogin = async (event) => {
-    event.preventDefault(); // Prevenir la recarga de la página
-
+    event.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3030/users/login', {
-        email,
-        password
-      });
+      const response = await axios.post('http://localhost:3030/users/login', { email, password });
       console.log('Login successful:', response.data);
-      // Aquí puedes almacenar el token recibido en localStorage o manejar el estado de autenticación
       localStorage.setItem('token', response.data.token);
       Swal.fire({
         icon: 'success',
@@ -28,8 +25,8 @@ const LogIn = () => {
       });
     } catch (error) {
       if (error.response) {
-        // El servidor respondió con un código de estado fuera del rango 2xx
         console.log('Error:', error.response.data);
+        setErrorMessage(error.response.data.message);
         Swal.fire({
           icon: 'error',
           title: 'Error de inicio de sesión',
@@ -38,7 +35,6 @@ const LogIn = () => {
           confirmButtonText: 'Intentar de nuevo'
         });
       } else {
-        // Algo salió mal en la configuración al enviar la solicitud
         console.log('Error:', error.message);
         Swal.fire({
           icon: 'error',
@@ -56,9 +52,7 @@ const LogIn = () => {
       <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
         <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
           <h1 className="title-font font-medium text-3xl text-gray-900">Inicio de Sesión Seguro</h1>
-          <p className="leading-relaxed mt-4">
-            Por favor, inicia sesión para continuar.
-          </p>
+          <p className="leading-relaxed mt-4">Por favor, inicia sesión para continuar.</p>
         </div>
         <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
           <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Log In</h2>
@@ -76,9 +70,9 @@ const LogIn = () => {
               />
             </div>
             <div className="relative mb-4">
-              <label htmlFor="password" className="leading-7 text-sm text-gray-600">Password</label>
+              <label htmlFor="password" className="leading-7 text-sm text-gray-600">Contraseña</label>
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 id="password"
                 name="password"
                 required
@@ -86,6 +80,12 @@ const LogIn = () => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 pt-6">
+                <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+              </button>
             </div>
             <button type="submit" className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Log In</button>
             {errorMessage && <p className="text-red-500 text-xs italic mt-3">{errorMessage}</p>}
