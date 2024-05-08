1c98c51592c94e315678d9a1e6dc53d2cdc7faf3
Jonathan Barroeta Lasa <jbarroeta@opendeusto.es>
2024-05-08 20:05:13 +0200

    reestructura de carpetas

--- frontend/src/components/Footer/footer.jsx
+++ frontend/src/components/Footer/footer.jsx
@@ -1,9 +1,11 @@
+import Logo from "../logo";
+
 const Footer = () => {
   return (
     <footer className="text-gray-600 body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <img src="src/img/ejercicio.png" className="mr-3 h-6 sm:h-9" alt="Logo" />
+          <Logo />
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
         </a>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
--- frontend/src/components/logo.jsx
+++ frontend/src/components/logo.jsx
@@ -1,6 +1,6 @@
 const Logo = () => {
     return (
-        <img src="src/img/ejercicio.png" className=" mr-3 h-10" alt="Logo" />
+        <img src="src/assets/icons/ejercicio.png" className=" mr-3 h-10" alt="Logo" />
     );
   };
   
